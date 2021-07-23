7cb02f2d52db1fd213d5b2b603c789defd554388
gethziyaljoyce <87714086+gethziyaljoyce@users.noreply.github.com>
2021-07-23 10:46:19 +0530

    Add files via upload

---
+++ sample1.js.js
@@ -0,0 +1,44 @@
+var technicalspecification = {
+    dimensions: {
+        overalllengthmm: 3500,
+        overallwidthmm: 1600,
+        overallheight: 1490,
+        wheelbasemm: 2360,
+        trackwidth: { front: 1405, rear: 1400 },
+        minimumturningradiusm: "4.5",
+        minimumgroundclearencemm: 170,
+    },
+    capacities: {
+        seatingcapacityPersons: 5,
+        fueltankcapacityLitres: 35,
+    },
+    engine: {
+        type: "kb-series",
+        numberofcylinder: 3,
+        nuberofvalves: 12,
+        capacityCCcm3: 998,
+        borexstroke: "73.0,79.5",
+        compressionratio: "10:1",
+        maximumpowerPSrpm: "67/6200",
+        maximumtorqueNmrpm: "90/3500",
+        fueldistribution: "multipointinjection"
+    },
+    transmission: {
+        type: "5 speed MT"
+    },
+    chassis: {
+        streering: "rack&pinion-power assisted"},
+        brakes: {
+             front: "ventilated discs", rear: "drums"
+             },
+        suspensions: {
+            front: "macperson strut&coil spring", rear: "isolated trailing link&coil spring"
+        },
+        shockAbservers: "gas filled",
+        tyreTubeless: "155/80R13",
+        weights: {
+            kerbWeightMinimumwithFulloptionskg: "860-880",
+            grossVehicleWeightkg: "1320"
+        }
+
+    }
